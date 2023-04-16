69078f61ba8c1ca063629de036b5c44761587c87
Wendymochi <wendiyprl@gmail.com>
2023-04-16 03:54:21 -0600

    Ya sirve tu splash papi

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:aplicacion/InicioSesion/SplashS.dart';
 import 'package:aplicacion/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-//Quieres ir al cine o a comer ahorita que termine la reunión bonita? 
+
+//Quieres ir al cine o a comer ahorita que termine la reunión bonita?
+
 void main (){
   runApp(
       const App()
